916c64075d110cf57ee52b17e93b9ca5dff54f83
aj045045 <aj045045@gmail.com>
2023-07-07 10:56:17 +0530

    add changes

--- Sem-4/Subjects/React-Department/routes/events.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('?id=', function (req, res, next) {
-    console.log(req.originalUrl);
-    console.log(req.query.id);
-});
-
-module.exports = router;
